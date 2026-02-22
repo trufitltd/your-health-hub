d34fcc402963196207bfe746e8e0975a683478d5
Ramadan <trufit.ltd.ng@gmail.com>
2026-02-22 13:14:54 +0100

    fixed patient message scrolling

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.787ndk2if7s"
+    "revision": "0.4t1f34i4olc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
