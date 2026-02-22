2643a0503489b7a4d47fa87ef47b4e991df319a9
Ramadan <trufit.ltd.ng@gmail.com>
2026-02-22 11:12:06 +0100

    modified PWA

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.fn9k3g1tk8s"
+    "revision": "0.787ndk2if7s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
