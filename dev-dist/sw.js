6fa8c1a108f8bece53a1ec58d4df47f39c0d645a
Ramadan <trufit.ltd.ng@gmail.com>
2026-03-31 20:56:23 +0100

    modified admin login

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.74n5ku4efug"
+    "revision": "0.p1h9lqn5og4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
