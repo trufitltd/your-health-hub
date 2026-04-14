84fe851d22851c4bedef571961d93941d4dc2810
Ramadan <trufit.ltd.ng@gmail.com>
2026-04-14 13:14:23 +0100

    added multiplatform share

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -80,7 +80,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.7e0h99648b8"
+    "revision": "0.rs3pp8ddlik"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
