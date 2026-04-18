2637d67410f7c0325da7293f874d1faa90707bfb
Ramadan <trufit.ltd.ng@gmail.com>
2026-04-18 15:04:30 +0100

    fixed push notification

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -80,7 +80,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.i2472n1guv4"
+    "revision": "0.tmmhareef0o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
