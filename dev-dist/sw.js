7c2f7f1ec01a3a7cc58813624a3f420f61ab3f79
Ramadan <trufit.ltd.ng@gmail.com>
2026-04-18 14:21:10 +0100

    fixed paystack

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -80,7 +80,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.a9huk84vku4"
+    "revision": "0.i2472n1guv4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
