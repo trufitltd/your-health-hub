86519c9dfed640598026ada3da8069c57fb6a32a
trufitltd <trufit.ltd.ng@gmail.com>
2026-06-25 21:16:58 +0100

    updated service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -80,7 +80,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.2uhnsqgvq2g"
+    "revision": "0.pficnv6305c"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
